6fc6678604d6cdfb29132becf4fb8a0a8cf689ad
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-05 02:05:16 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  app.enableCors()
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
