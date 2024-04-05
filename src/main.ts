a10627088ef40437405ecca2eac167d05940423d
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-05 11:26:07 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'https://660f92ba7125144e20f04958--lucky-croissant-cf361b.netlify.app/',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: '*', 
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
     credentials: true,
-    maxAge: 3600,
-    
   });
   await app.listen(3000);
 }
