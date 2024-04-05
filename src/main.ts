f6035f256e14b4edcdb8abd2c31a4547a34f238b
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-05 02:58:20 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://127.0.0.1:5500/index.html',
+    origin: 'https://660f92ba7125144e20f04958--lucky-croissant-cf361b.netlify.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: '*', 
     preflightContinue: false,
