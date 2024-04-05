dbc98794e93e4ccceaeab6cb6f3411ed746be0d5
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-05 11:43:29 -0300

    middle

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,16 @@ async function bootstrap() {
     allowedHeaders: '*', 
     credentials: true,
   });
+
+  // Manajador de opciones CORS
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+
   await app.listen(3000);
 }
 bootstrap();
